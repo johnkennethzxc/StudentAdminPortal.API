3be4438f611b473e5b14fdf6c6b1033c13560ca3
johnkennethzxc <bombanejohnkenneth01@gmail.com>
2022-04-19 20:46:37 +0800

    End of section 8

--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -68,6 +68,20 @@ namespace StudentAdminPortal.API.Repositories
                 .ToListAsync();
         }
 
+        public async Task<bool> UpdateProfileImage(Guid studentId, string profileImageUrl)
+        {
+            var student = await GetStudentAsync(studentId);
+
+            if (student != null)
+            {
+                student.ProfileImageUrl = profileImageUrl;
+                await context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
         public async Task<Student> UpdateStudent(Guid studentId, Student request)
         {
             var existingStudent = await GetStudentAsync(studentId);
