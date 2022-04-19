e4c9820acfe9caa48728fa0d1dcbe435689e60d5
johnkennethzxc <bombanejohnkenneth01@gmail.com>
2022-04-19 22:16:31 +0800

    End of section 9

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Validators/AddStudentRequestValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Validators
+{
+    public class AddStudentRequestValidator : AbstractValidator<AddStudentRequest>
+    {
+        public AddStudentRequestValidator(IStudentRepository studentRepository)
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.DateOfBirth).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Mobile).LessThan(9999999999).GreaterThan(8888888888);
+            RuleFor(x => x.GenderId).NotEmpty().Must(id =>
+            {
+                var gender = studentRepository
+                    .GetGendersAsync()
+                    .Result
+                    .ToList()
+                    .FirstOrDefault(x => x.Id == id);
+
+                if (gender != null)
+                {
+                    return true;
+                }
+
+                return false;
+            }).WithMessage("Please select a valid gender.");
+            RuleFor(x => x.PhysicalAddress).NotEmpty();
+            RuleFor(x => x.PostalAddress).NotEmpty();
+        }
+    }
+}
